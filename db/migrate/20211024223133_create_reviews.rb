1794ab9bba798914d0b94eda9862a196e0196a2d
Antonio <atcarter005@gmail.com>
2021-10-24 17:31:53 -0500

    rails g resource review

---
+++ db/migrate/20211024223133_create_reviews.rb
@@ -0,0 +1,12 @@
+class CreateReviews < ActiveRecord::Migration[6.1]
+  def change
+    create_table :reviews do |t|
+      t.string :username
+      t.string :content
+      t.integer :rating
+      t.belongs_to :movie, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
