d4ed7d3886ae927ffef41cb10412d082560c7be3
Antonio <atcarter005@gmail.com>
2021-10-27 04:59:04 -0500

    Add show, create and destroy methods for reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,4 +3,39 @@ class ReviewsController < ApplicationController
     reviews = Review.all
     render json: reviews
   end
+
+  def show
+    if the_review_exists?
+      review = Review.find_by_id(params[:id])
+      render json: review
+    else
+      render json: {error: "Review not found."}
+    end
+  end
+
+  def create
+    review = Review.new(review_params)
+
+    if review.save
+      render json: review
+    else
+      render json: {error: "Review not saved."}
+    end
+  end
+
+  def destroy
+    review = Review.find_by_id(params[:id])
+    review.destroy
+
+    render json: {alert: "The review was deleted."}
+  end
+
+  private 
+  def the_review_exists?
+    params[:id] && Review.exists?(params[:id])
+  end
+
+  def review_params
+    params.require(:review).permit(:username, :content, :rating, :movie_id)
+  end
 end
