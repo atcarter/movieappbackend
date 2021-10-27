1f3d24043c0fcc4ffc113714b91e08aee8c28ae2
Antonio <atcarter005@gmail.com>
2021-10-26 19:31:10 -0500

    Add AR validations to movie model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,14 @@
 class Movie < ApplicationRecord
   has_many :reviews
+
+  validates :title, presence: true
+  validates :synopsis, length: { in: 20..300}
+  validates :year, numericality: { greater_than: 1890 }
+  before_validation :titlecase
+
+  private
+
+  def titlecase
+    self.title = self.title.titleize
+  end
 end
