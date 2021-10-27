fe39d4a95e4772e407cbb6acb9a6991ef2923727
Antonio <atcarter005@gmail.com>
2021-10-27 04:51:34 -0500

    Added create and destroy methods to movie model

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -10,15 +10,35 @@ class MoviesController < ApplicationController
       movie = Movie.find_by_id(params[:id])
       render json: movie
     else
-      message = "Movie not found."
-      render json: {error: message}
+      render json: {error: "Movie not found."}
     end
+  end
+
+  def create
+    movie = Movie.new(movie_params)
+
+    if movie.save
+      render json: movie
+    else
+      render json: {error: "Movie not saved."}
+    end
+  end
+
+  def destroy
+    movie = Movie.find_by_id(params[:id])
+    movie.destroy
+
+    render json: {alert: "Movie was deleted."}
   end
 
   private
 
   def the_movie_exists?
     params[:id] && Movie.exists?(params[:id])
+  end
+
+  def movie_params
+    params.require(:movie).permit(:title, :year, :synopsis)
   end
 
 end
