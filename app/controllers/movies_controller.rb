019da8ed6b84d63e6d0f4961004b0f8de4844ec7
Antonio <atcarter005@gmail.com>
2021-10-27 00:21:08 -0500

    add show method to movies controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,24 @@
 class MoviesController < ApplicationController
+
   def index
     movies = Movie.all
     render json: movies
   end
+
+  def show
+    if the_movie_exists?
+      movie = Movie.find_by_id(params[:id])
+      render json: movie
+    else
+      message = "Movie not found."
+      render json: {error: message}
+    end
+  end
+
+  private
+
+  def the_movie_exists?
+    params[:id] && Movie.exists?(params[:id])
+  end
+
 end
